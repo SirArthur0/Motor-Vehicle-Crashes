18f1dd670312c1407e010c29257171ce8e65d214
Arthur Machado <amachado@CORP2000.Org>
2025-07-26 23:33:51 -0300

    WIP-dataAnalysis.py

---
+++ dataAnalysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pandas import read_csv
+
+pd.options.display.max_rows = 10
+pd.options.display.max_columns = 20
+
+
+file_name = 'Motor Vehicle Crash.csv'
+
+df = pd.read_csv(file_name)
+
+# Convert from Military Time to AM/PM format
+df['Time AMPM'] = pd.to_datetime(df['Time'], format='%H:%M').dt.strftime('%I:%M %p')
+df.drop(columns='Time', inplace=True)
+df.rename(columns={'Time AMPM':'Time'}, inplace=True)
+
+
+# --- Remove comment to check data info  --- #
+
+# See df general info
+#print(df.info())
+
+# See df shape
+#print(df.shape)
+
+# Remove comment to see sum of null by column
+#pd.options.display.max_rows = 18
+#print(df.isnull().sum())
+
+# remove comment to see period range
+#print("From:", df['Date'].min(), "To:", df['Date'].max())
+
+# Save df into new csv file 
+df.to_csv("M V C.csv", index = False)
